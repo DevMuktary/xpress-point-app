5dbda40c1c57de0669ef38a825510c25d876f784
DevMuktary <mukhtarakanniabdulwaheed@gmail.com>
2025-11-09 10:43:02 +0100

    Update slipGenerator.ts

--- lib/slipGenerator.ts
+++ lib/slipGenerator.ts
@@ -181,9 +181,9 @@ export async function generateNinSlipPdf(slipType: string, data: any): Promise<B
     
     // --- THIS IS THE FIX (Photo) ---
     // Moved down 100 (y: height - 714 -> height - 814)
-    // Moved left 10 (x: 197 -> 187)
+    // Moved left 15 (x: 187 -> 172)
     page.drawImage(userPhoto, { 
-      x: 187, 
+      x: 172, 
       y: height - (550 + 164 + 100), // y: height - 814
       width: 264, 
       height: 328 
