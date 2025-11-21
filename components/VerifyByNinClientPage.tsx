9a55739a6ebf2de694f254a759c4b5789d2f3227
DevMuktary <mukhtarakanniabdulwaheed@gmail.com>
2025-11-21 06:55:34 +0100

    Update VerifyByNinClientPage.tsx

--- components/VerifyByNinClientPage.tsx
+++ components/VerifyByNinClientPage.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { IdentificationIcon, InformationCircleIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { IdentificationIcon, InformationCircleIcon, ChevronLeftIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import Loading from '@/app/loading';
 import SafeImage from '@/components/SafeImage';
 
