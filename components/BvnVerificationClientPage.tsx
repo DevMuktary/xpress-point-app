a29c971bff5bfc85de55215dbedb32696d164a83
DevMuktary <mukhtarakanniabdulwaheed@gmail.com>
2025-12-01 12:27:15 +0100

    Update BvnVerificationClientPage.tsx

--- components/BvnVerificationClientPage.tsx
+++ components/BvnVerificationClientPage.tsx
@@ -7,6 +7,7 @@ import {
   XMarkIcon,
   InformationCircleIcon,
   ShieldCheckIcon,
+  CheckCircleIcon,
   DocumentTextIcon
 } from '@heroicons/react/24/outline';
 import Loading from '@/app/loading';
