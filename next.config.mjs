a0f338cf12bdc814af31be8d273391f6dc618530
MUKTAR ABDULWAHEED <mukhtarakanniabdulwaheed@gmail.com>
2026-05-19 08:13:40 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone', // CRITICAL FIX: Compiles a lightweight Node server for Railway
 };
 
 export default nextConfig;
